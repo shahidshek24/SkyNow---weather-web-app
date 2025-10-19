0fbd455ace13e8cdea6cce917e077c6aef7612b4
shahidshek24 <shekshahid2403@gmail.com>
2025-10-19 23:40:36 +0530

    fisrt version

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import "./App.css";
-import CurrentLocationWeather from "./components/CurrentLocationWeather";
-import SearchCityWeather from "./components/SearchCityWeather";
+import './App.css'
+import CurrentLocationWeather from './components/CurrentLocationWeather'
+import SearchCityWeather from './components/SearchCityWeather'
 
 export default function App() {
   return (
     <div className="App">
+      {/* 🪙 Header with Logo and Title */}
+      <header className="app-header">
+        <img src="/logo1.png" alt="App Logo" className="app-logo" />
+      </header>
+
+      {/* 🌤 Weather Cards Container */}
       <div className="weather-container">
-        
-        <SearchCityWeather />
         <CurrentLocationWeather />
+        <SearchCityWeather />
       </div>
     </div>
-  );
+  )
 }
